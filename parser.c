92a29464320db0589bf2b88c26ca05d6e1956cb9
Alyssa Rosenzweig <alyssa@collabora.com>
2021-09-19 13:10:46 -0400

    init

--- parser.c
+++ parser.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <errno.h>
 
+#define round_up(x, y) ((x + (y - 1)) & ~(y - 1))
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -12,6 +13,11 @@ struct ctx {
 	void *blob;
 	u32 pos, len;
 };
+
+int parse_obj(struct ctx *ctx)
+{
+	/* Align to 32-bits */
+	ctx->pos = round_up(ctx->pos, 4);
 
 int parse(void *blob, size_t size)
 {
@@ -29,6 +35,10 @@ int parse(void *blob, size_t size)
 
 	if (*header != OSSERIALIZE_HDR)
 		return -EINVAL;
+
+	ctx.pos += sizeof(*header);
+
+	parse_obj(&ctx);
 
 	return 0;
 }
